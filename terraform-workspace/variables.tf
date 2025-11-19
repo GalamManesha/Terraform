83fb2189ed806ad7e3a41ce8b698b292e498f7ef
GalamManesha <maneeshagalam689@gmail.com>
2025-11-19 17:37:49 +0530

    Create variables.tf

---
+++ terraform-workspace/variables.tf
@@ -0,0 +1,29 @@
+variable "instance_type" {
+  type    = string
+  default = "t3.small"
+}
+
+variable "key_name" {
+  type    = string
+  default = "connect"
+}
+
+variable "ami" {
+  type    = string
+  default = "ami-02b8269d5e85954ef"
+}
+
+variable "tags" {
+  type    = string
+  default = "variables-hello"
+}
+
+variable "region" {
+  type    = string
+  default = "ap-south-1"
+}
+variable "aws_s3_bucket" {
+    default = "my-first-bucket-state-file"
+    type = string
+  
+}
